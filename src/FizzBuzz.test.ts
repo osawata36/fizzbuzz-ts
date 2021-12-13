b74d606d281e78efc23f119194b98578650ad901
OsaWatanabe <sr400.xj1jr@gmail.com>
2021-12-13 14:35:36 +0900

    失敗するテストを追加

--- src/FizzBuzz.test.ts
+++ src/FizzBuzz.test.ts
@@ -3,5 +3,9 @@ import { FizzBuzz  } from './FizzBuzz';
 test('1を渡すと文字列1を返す', () => {
   const fizzbuzz = new FizzBuzz()
   expect(fizzbuzz.convert(1)).toBe('1')
-});
+})
 
+test('3を渡すと文字列Fizzを返す', () => {
+  const fizzbuzz = new FizzBuzz()
+  expect(fizzbuzz.convert(3)).toBe('Fizz')
+})
