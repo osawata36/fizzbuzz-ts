a8a76dbd25c93a2c71adf8a0c10d7d3dbe902f4e
OsamuWatanabe <sr400.xj1jr@gmail.com>
2022-04-25 23:02:06 +0900

    2のテストケース追加

--- src/FizzBuzz.test.ts
+++ src/FizzBuzz.test.ts
@@ -6,14 +6,18 @@ beforeEach(() => {
   fizzbuzz = new FizzBuzz()
 })
 
-test('1を渡すと文字列1を返す', () => {
+test("1を渡すと文字列'1'を返す", () => {
   expect(fizzbuzz.convert(1)).toBe('1')
 })
 
-test('3を渡すと文字列Fizzを返す', () => {
+test("2を渡すと文字列'2'を返す", () => {
+  expect(fizzbuzz.convert(2)).toBe('2')
+})
+
+test("3を渡すと文字列'Fizz'を返す", () => {
   expect(fizzbuzz.convert(3)).toBe('Fizz')
 })
 
-test('5を渡すと文字列Buzzを返す', () => {
+test("5を渡すと文字列'Buzz'を返す", () => {
   expect(fizzbuzz.convert(5)).toBe('Buzz')
 })
